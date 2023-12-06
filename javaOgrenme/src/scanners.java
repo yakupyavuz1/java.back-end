12e052e1b21fcfef5f459bf461438d1d818d64b7
Yakup Yavuz <77230984+yakupyavuz1@users.noreply.github.com>
2023-12-06 23:10:16 +0300

    Update scanners.java

--- javaOgrenme/src/scanners.java
+++ javaOgrenme/src/scanners.java
@@ -1,14 +1,1 @@
-import java.util.Scanner ;
 
-public class scanners {
-    public static void main (String[] args) {
-
-        int a  ;
-
-        Scanner input = new Scanner(System.in);
-        a = input.nextInt() ;
-        System.out.println(a);
-
-    }
-
-}
